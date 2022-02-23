8b3d2bbc38c68789ab1abf07d7baf3e7a5c8a742
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-23 04:07:59 +0300

    Complete tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
-from hypothesis import (HealthCheck,
-                        settings)
+import os
+from datetime import timedelta
 
+from hypothesis import settings
+
+on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
+max_examples = settings.default.max_examples
 settings.register_profile('default',
-                          deadline=None,
-                          suppress_health_check=[HealthCheck.filter_too_much,
-                                                 HealthCheck.too_slow])
+                          deadline=(timedelta(hours=1) / max_examples
+                                    if on_azure_pipelines
+                                    else None),
+                          max_examples=max_examples)
